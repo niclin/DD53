0cd22c5d2cade1af3e0e37fc611dbe3313d745ec
niclin <bboyceo@hotmail.com>
2016-05-24 17:07:55 +0800

    fix

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -1,5 +1,4 @@
 class Admin::EventsController < ApplicationController
-  require 'slack-notifier'
   layout "admin"
 
   def index
@@ -25,10 +24,6 @@ class Admin::EventsController < ApplicationController
       @event = Event.create(menu_id: params[:menu_id], user_id: @user, date: @date, menu_name: @menu.title)
       @event.status_open
       redirect_to :back, alert: "已開啟#{@menu.title}的訂餐，快通知大家吧！"
-
-      notifier = Slack::Notifier.new "https://hooks.slack.com/services/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"
-      notifier.ping "<a href="http://202.168.197.185/">已開啟#{@menu.title}的訂餐，快來點餐吧</a>"
-
     end
   end
 
