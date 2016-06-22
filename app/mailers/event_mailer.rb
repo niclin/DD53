57f7e73d34d66ea472ff709faecd1d216405ec6e
niclin <bboyceo@hotmail.com>
2016-06-22 20:48:05 +0800

    Fixed mailer css style not working

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -1,8 +1,7 @@
 class EventMailer < ApplicationMailer
+ include Roadie::Rails::Automatic
   def notify_event_completed(event)
     @orders = event.orders
-
     mail(to: 'nic@kt.net.tw' , subject: "[DD53] 訂購「#{event.menu_name}」餐點的團體明細" )
-
   end
 end
