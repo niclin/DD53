b2f02ca2f59c3e719ee161a53769c11e5d70311d
niclin <bboyceo@hotmail.com>
2016-06-22 22:33:35 +0800

    try to reslove mailer error

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -1,5 +1,6 @@
 class EventMailer < ApplicationMailer
- include Roadie::Rails::Automatic
+  self.asset_host = nil
+  include Roadie::Rails::Automatic
   def notify_event_completed(event)
     @orders = event.orders
     mail(to: 'nic@kt.net.tw' , subject: "[DD53] 訂購「#{event.menu_name}」餐點的團體明細" )
