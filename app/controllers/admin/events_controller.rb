8c4a6ae8460081c3cbdaa6c06d9126e3a26971f0
niclin <bboyceo@hotmail.com>
2016-06-24 17:59:44 +0800

    Fix menus bug

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -22,7 +22,7 @@ class Admin::EventsController < ApplicationController
     @user = current_user.id
     @date = DateTime.now
     if request.post?
-      @event = Event.create(menu_id: params[:menu_id], user_id: @user, date: @date, menu_name: @menu.title)
+      @event = Event.create(menu_id: @menu.id, user_id: @user, date: @date, menu_name: @menu.title)
       @event.status_open
       redirect_to :back, alert: "已開啟#{@menu.title}的訂餐，快通知大家吧！"
       if Rails.env.production?
