6a3f4db93c077cc715fd9d72971ce2c9d000e06b
niclin <bboyceo@hotmail.com>
2016-06-26 17:58:37 +0800

    Update food item use ajax when select quantity

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,3 +1,7 @@
-  $(document).ready(function(){
-    $("#cart-inner").sticky({topSpacing:0});
+$(function() {
+  return $(document).on('change', '#cart_item_quantity', function(evt) {
+    $('[id^=edit_cart_item] #cart_item_quantity').on('blur', function() {
+      $(this).parents('form').submit();
+    });
   });
+});
