6dc0b493f579565609e7a87b2b197bac67faf6f1
niclin <bboyceo@hotmail.com>
2016-07-28 10:39:52 +0800

    find team

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,4 +1,15 @@
 class TeamsController < ApplicationController
+  before_action :find_this_team
+
   def show
+  end
+
+  protected
+
+
+  def find_this_team
+
+    @team = current_user.teams.find_by_domain(request.host.split('.').first)
+    # make sure with domain
   end
 end
