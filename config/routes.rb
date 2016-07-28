0acae3ed209b89d392f4951c83a739f43f75c082
niclin <bboyceo@hotmail.com>
2016-07-28 10:22:20 +0800

    Add name input to devise view

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   as :user do
     patch '/user/confirmation' => 'confirmations#update', :via => :patch, :as => :update_user_confirmation
   end
-  devise_for :users, :controllers => { :confirmations => "confirmations" }
+  devise_for :users, :controllers => { :confirmations => "confirmations", registrations: 'registrations' }
 
   namespace :account do
     resources :orders
