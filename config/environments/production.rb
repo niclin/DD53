d0d24c743d57a631def13d58a1d7718cb67e96ea
niclin <bboyceo@hotmail.com>
2016-07-29 13:03:53 +0800

    Add mailer default host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,6 +79,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = config_for(:email).symbolize_keys
+  config.action_mailer.default_url_options = { host: 'dd53.xyz' }
 
 
   Rails.application.config.middleware.use ExceptionNotification::Rack,
