7f8dc8fdd46197c04393def6c2b93fad4ac40e43
niclin <bboyceo@hotmail.com>
2016-07-30 23:48:00 +0800

    Update if user sing in not exist team, auto redirect to right team

--- app/controllers/concerns/find_team.rb
+++ app/controllers/concerns/find_team.rb
@@ -4,5 +4,9 @@ module FindTeam
   protected
   def find_this_team
     @team = current_user.teams.find_by_domain(request.host.split('.').first)
+    #不是群組的人請你滾回自己的群組
+    if @team.blank? and !current_user.teams.empty?
+      redirect_to root_url(:subdomain => current_user.teams.last.domain)
+    end
   end
 end
