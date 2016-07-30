0bf25b165f1e40823a32d5283106bbe6ea5e8732
niclin <bboyceo@hotmail.com>
2016-07-31 00:36:12 +0800

    Fix bug

--- app/controllers/concerns/find_team.rb
+++ app/controllers/concerns/find_team.rb
@@ -6,7 +6,8 @@ module FindTeam
     @team = current_user.teams.find_by_domain(request.host.split('.').first)
     #不是群組的人請你滾回自己的群組
     if @team.blank? and !current_user.teams.empty?
-      redirect_to root_url(:subdomain => current_user.teams.last.domain)
+      redirect_to signout_path(:subdomain => false)
+      flash[:warning] = "登入錯誤的群組"
     end
   end
 end
