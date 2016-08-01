10d17389f0d0bf006cf940e6632b01428d24a1d7
niclin <bboyceo@hotmail.com>
2016-08-01 10:29:33 +0800

    Move rotues in subdomain

--- config/routes.rb
+++ config/routes.rb
@@ -13,52 +13,50 @@ Rails.application.routes.draw do
 
   constraints(Subdomain) do
     get '/' => 'events#index'
-  end
-
-  namespace :account do
-    resources :orders
-  end
-
-  namespace :admin do
-    resources :menus
-    resources :events do
-      post :invoice
+    namespace :account do
+      resources :orders
     end
-    resources :orders
-    resources :users do
-      member do
-        post :to_admin
-        post :to_normal
+    namespace :admin do
+      resources :menus
+      resources :events do
+        post :invoice
+      end
+      resources :orders
+      resources :users do
+        member do
+          post :to_admin
+          post :to_normal
+        end
+      end
+      match 'event/select', to: 'events#select', as: :select, via: [:get, :post]
+      match 'event/select/:menu_id', to: 'events#select_menu', as: :select_menu, via: [:get, :post]
+      match 'user/invite', to: 'users#invite', as: :invite, via: [:get, :post]
+    end
+
+    resources :carts do
+      collection do
+        post :checkout
+        delete :clean
       end
     end
-    match 'event/select', to: 'events#select', as: :select, via: [:get, :post]
-    match 'event/select/:menu_id', to: 'events#select_menu', as: :select_menu, via: [:get, :post]
-    match 'user/invite', to: 'users#invite', as: :invite, via: [:get, :post]
-  end
 
-  resources :carts do
-    collection do
-      post :checkout
-      delete :clean
+    resources :items, controller: "cart_items"
+    resources :events do
+      member do
+        post :join_event
+        get :ordering
+        get :reference
+        post :to_abandon
+      end
+      match 'cancel_order/:order_id', to: 'events#cancel_order', as: :cancel_order, via: [:post]
+      match 'food/:food_id/add_to_cart', to: 'events#add_to_cart', as: :add_to_cart, via: [:get, :post]
+      match 'food/:food_id/:sub_id/add_multi_to_cart', to: 'events#add_multi_to_cart', as: :add_multi_to_cart, via: [:get, :post]
     end
+
+
+    resources :menus
+    resources :orders
   end
-
-  resources :items, controller: "cart_items"
-  resources :events do
-    member do
-      post :join_event
-      get :ordering
-      get :reference
-      post :to_abandon
-    end
-    match 'cancel_order/:order_id', to: 'events#cancel_order', as: :cancel_order, via: [:post]
-    match 'food/:food_id/add_to_cart', to: 'events#add_to_cart', as: :add_to_cart, via: [:get, :post]
-    match 'food/:food_id/:sub_id/add_multi_to_cart', to: 'events#add_multi_to_cart', as: :add_multi_to_cart, via: [:get, :post]
-  end
-
-
-  resources :menus
-  resources :orders
 
   root 'pages#welcome'
 
