dda6810f80543939d2f47a935e7b835bed0590c4
niclin <bboyceo@hotmail.com>
2016-08-01 12:22:48 +0800

    Redirect all unknown routes to root_url

--- config/routes.rb
+++ config/routes.rb
@@ -65,6 +65,9 @@ Rails.application.routes.draw do
   match "/404", :to => "errors#not_found", :via => :all
   match "/500", :to => "errors#internal_server_error", :via => :all
 
+  # redirect all unknown routes to root_url
+  get '*path' => redirect('/')
+
   root 'pages#welcome'
 
 end
