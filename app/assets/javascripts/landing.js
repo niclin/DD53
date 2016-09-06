f99e57b1e8f085effa65e75717c19985bf746e5b
niclin <bboyceo@hotmail.com>
2016-09-06 17:00:19 +0800

    Update some code

--- app/assets/javascripts/landing.js
+++ app/assets/javascripts/landing.js
@@ -12,6 +12,9 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap
+//= require bootstrap/dropdown
+//= require bootstrap/collapse
+//= require bootstrap/modal
+//= require bootstrap/alert
 
 
